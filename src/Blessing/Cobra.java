0f02d57e0a47d56ddf1e7ad221e2f479fc844f7d
IBOUKOUN <blessinglovejob@gmail.com>
2022-12-26 17:05:38 -0600

    zoo-app User

---
+++ src/Blessing/Cobra.java
@@ -0,0 +1,11 @@
+package Blessing;
+
+public class Cobra {
+       public void  typeOfCobra(){
+            String[] cobratype = {"Naja cobra", "Ophiophagus the king cobra", "Aspidelaps: Shield-nose cobras",
+                    "Pseudohaje: Tree cobras", "Walterinnesia: black desert cobras"};
+            for (String i : cobratype) {
+                System.out.println(i);
+           }
+        }
+}
---
+++ src/Blessing/Lion.java
@@ -0,0 +1,11 @@
+package Blessing;
+
+public class Lion {
+
+      public  void lionType() {
+          String[] typeOfLions = {"Asiatic lion", "African lion", "Katanga lion", "Masai lion", "Abyssinian lion"};
+          for (String i : typeOfLions) { //for each loop
+              System.out.println(i);
+
+          }
+      }}
